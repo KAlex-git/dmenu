32edb8e83339275580762d0accfc2344405e561d
KAlex-git <Kalash20150825@outlook.com>
2023-12-16 12:00:58 +0200

    fork dmenu for me

---
+++ theme/Old1.h
@@ -0,0 +1,16 @@
+static const char *colors[SchemeLast][2] = {
+	/*                            fg         bg       */
+
+	[SchemeNorm]            = { "#eeeeee", "#000000" },
+	[SchemeSel]             = { "#afff2f", "#0b4199" },
+	[SchemeSelHighlight]    = { "#d7d7d7", "#000000" },
+	[SchemeNormHighlight]   = { "#e78400", "#000000" },
+	[SchemeOut]             = { "#000000", "#00ffff" },
+	[SchemeMid]             = { "#444444", "#1f2026" },
+};
+
+/*
+   SchemeNorm fg - цвет невыделенного объекта, а также набираемого текста
+   SchemeNorm bg - фон меню
+
+*/
